32f0f566817b654b5941528595070a7cb72070e0
Sai <suryasaigopal@outlook.com>
2019-03-01 06:54:50 +0000

    FizzBuzz program Initial Script

---
+++ AZTestWeb/AZTestWeb/Controllers/FizzBuzzController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AZTestWeb.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        // GET: FizzBuzz
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

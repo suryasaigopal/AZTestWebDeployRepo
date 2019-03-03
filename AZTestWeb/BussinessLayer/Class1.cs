17ad62dc93c741f0f14349e999b87e2cdcb7463e
Sai <suryasaigopal@outlook.com>
2019-03-03 14:52:34 +0000

    Implementation of FizzBuzz

---
+++ AZTestWeb/BussinessLayer/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer
+{
+    public class Class1
+    {
+        public List<string> logicFizzBuzz()
+        {
+            List<string> lstfizzbuzz = new List<string>();
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    lstfizzbuzz.Add("Fizz Buzz");
+                else if (i % 3 == 0)
+                    lstfizzbuzz.Add("Fizz");
+                else if (i % 5 == 0)
+                    lstfizzbuzz.Add("Buzz");
+                else 
+                    lstfizzbuzz.Add(i.ToString());
+                
+            }
+            return lstfizzbuzz;
+        }
+
+    }
+}
---
+++ AZTestWeb/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BussinessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        FizzbuzzClass oFizz = new FizzbuzzClass();
+        List<string> oList = new List<string>();
+        [TestMethod]
+        public void NotFizzBuzzCase()
+        {
+            oList = oFizz.logicFizzBuzz();
+            Assert.AreEqual(oList[1].ToString(),"2");
+        }
+
+
+        [TestMethod]
+        public void FizzCase()
+        {
+            oList = oFizz.logicFizzBuzz();
+            Assert.AreEqual(oList[14].ToString(),"Fizz Buzz");
+        }
+
+        [TestMethod]
+        public void BuzzCase()
+        {
+            oList = oFizz.logicFizzBuzz();
+            Assert.AreEqual(oList[4].ToString(),"Buzz");
+        }
+
+    }
+}
